3001a3eecea33deed0a607e3c701c957484f3221
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-12-28 18:14:50 +0700

    Update water.glsl

--- shaders/include/water.glsl
+++ shaders/include/water.glsl
@@ -4,5 +4,11 @@
 #include "include/pbr.glsl"
 
 vec3 waterBRDF(vec3 N, vec3 V, vec3 L, vec3 skyCol) {
-  float rough = clamp(0.04 / max(WATER_GLOSS, 0.001), 0.1, 0.2)
+  float rough = clamp(0.04 / max(WATER_GLOSS, 0.001), 0.1, 0.2);
+
+  vec3 col = BRDF_GGX(N, V, L vec3(0.02), 0.0, rough);
+
+  return col * skyCol;
 }
+
+#endif
